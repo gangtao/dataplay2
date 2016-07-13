d85a97f6689bb1cfd5b56806baff1d5be79e81eb
linxiaohui <llinxiaohui@126.com>
2016-07-13 11:36:31 +0800

    fix for Python3

--- package/main.py
+++ package/main.py
@@ -41,7 +41,7 @@ def csvdata():
         return json.dumps({error:
                            'You are not allowed to upload such a file.'})
     else:
-        flist = [f.replace('./data/', '').replace('.csv', '')
+        flist = [f.replace('./data'+os.sep, '').replace('.csv', '')
                  for f in glob.glob('./data/*.csv')]
         return json.dumps(flist)
 
@@ -66,7 +66,7 @@ def getdata(dataname):
             if headerOnly:
                 return jsonify(name=dataname, csv=csvfile.readline())
             else:
-                return jsonify(name=dataname, csv=csvfile.read())
+                return jsonify(name=dataname, csv=csvfile.read().decode("UTF-8"))
 
 
 @app.route('/ml/cls/<action>', methods=['GET'])
